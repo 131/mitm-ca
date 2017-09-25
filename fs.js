51e1f01cbc4a8cf1d42dbb96a87f79a1d9d90f37
François Leurent <fleurent@ivsweb.com>
2017-09-26 00:37:41 +0200

    With a typo

--- fs.js
+++ fs.js
@@ -4,7 +4,7 @@ const fs   = require('fs');
 const path = require('path');
 const mkdirpSync = require('nyks/fs/mkdirpSync');
 
-const CA = require('./core');
+const CA = require('.');
 
 class CAfs extends CA {
   constructor(CAfolder)  {
